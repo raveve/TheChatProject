b2db42849e839e4e524566af5c46bf12bbccfaf6
Rachel <BlantonAlmeida@gmail.com>
2015-02-09 00:02:54 -0500

    updated styling and added to javascript, created date function and edited delete function

--- main.js
+++ main.js
@@ -26,24 +26,12 @@ var chat = {
       console.log("submit working");
 
       var newMessage = {
-        user: $(this).find('input[name="userName"]').val(),
-        message: $(this).find('input[name="newMessage"]').val()
+        user: $(this).find('input[name="userName"]').val() + ' \- ' + Date(),
+        message: $(this).find('input[name="newMessage"]').val(),
       };
-<<<<<<< HEAD
-
-      console.log(newMessage);
 
       chat.createMessage(newMessage);
 
-=======
-
-      chat.createMessage(newMessage);
-
-      function populatestorage(newMessage) {
-        localStorage.setItem(newMessage);
-        console.log("my local storage")
-      }
->>>>>>> f27e8f4a1a7ea158eaa462c3e3d10ffac75eb93a
     });
 
     $('section').on('click', '.delete-message', function (event) {
@@ -55,7 +43,7 @@ var chat = {
   },
 
   config: {
-    url: 'http://tiy-fee-rest.herokuapp.com/collections/snoopy',
+    url: 'http://tiy-fee-rest.herokuapp.com/collections/snoopy7',
   },
 
   render: function (data, tmpl, $el) {
@@ -94,24 +82,11 @@ var chat = {
         console.log(data);
         chat.renderMessages();
 
-<<<<<<< HEAD
-//TESTING//
-
-      var uid = ("0000" + (Math.random()*Math.pow(36,4) << 0).toString(36)).slice(-4);
-       console.log(uid);
-
-       localStorage.setItem(uid, JSON.stringify(data));
-       var restoredSession = JSON.parse(localStorage.getItem(uid));
-       console.log(restoredSession);
-
-
-=======
         var uid = ("0000" + (Math.random()*Math.pow(36,4) << 0).toString(36)).slice(-4);
         console.log(uid);
 
         localStorage.setItem(uid, JSON.stringify(data));
         var restoredSession = JSON.parse(localStorage.getItem(uid));
->>>>>>> f27e8f4a1a7ea158eaa462c3e3d10ffac75eb93a
 
         // Clears the message field on submit
         $('input.message-input').val('');
